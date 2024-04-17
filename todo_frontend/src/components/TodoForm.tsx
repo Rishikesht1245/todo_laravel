b792b5d95ea1df04ca0d3f298df9d6b24e067f80
Rishikesh T <rishikeshtgit@gmail.com>
2024-04-17 12:14:48 +0530

    Fix : resolve bugs in todo form and changed the logo

--- todo_frontend/src/components/TodoForm.tsx
+++ todo_frontend/src/components/TodoForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { createTodoAPI, updateTodoAPI } from "../apiRoutes/api";
 import toast from "react-hot-toast";
-import { ITodo, ITodoAddResponse } from "../interfaces/todo";
+import { ITodo } from "../interfaces/todo";
 import { useFormik } from "formik";
 import { todoSchema } from "../schema/todoSchema";
 import Input from "./Input";
@@ -9,57 +9,63 @@ import Button from "./Button";
 import { getUserID } from "../utils/helpers";
 import { useAppDispatch, useAppSelector } from "../App";
 import { fetchTodosByUserID } from "../store/todoSlice";
-const TodoForm = ({type, data, setModalOpen}: TodoFormProps) => {
+const TodoForm = ({ type, data, setModalOpen }: TodoFormProps) => {
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const dispatch = useAppDispatch();
+  const userId =
+    useAppSelector((state) => state?.auth?.user?.id) || getUserID();
 
-    const [errorMessage, setErrorMessage] = useState<string>("");
-    const dispatch = useAppDispatch();
-    const userId =useAppSelector((state) => state?.auth?.user?.id) || getUserID();
-
-    const handleSubmit = async (values : {
-        todo: string;
-        user_id : string;
-    }) => {
-        if(type == "add"){
-            try {
-                const response = await createTodoAPI(values);
-                toast.success(response?.data?.message);
-                dispatch(fetchTodosByUserID(userId));
-                setModalOpen(false);
-                
-            } catch (error) {
-                console.log("Error in todo creation", error);
-                setErrorMessage("Something went wrong!");
-            }
-
-        }else if(type == "edit"){
-            try {
-                const response = await updateTodoAPI(values, data?.id!);
-                toast.success(response?.data?.message);
-                dispatch(fetchTodosByUserID(userId));
-                setModalOpen(false);
-            } catch (error) {
-                console.log("Error in todo creatigon", error);
-                setErrorMessage("Something went wrong!");
-            }
-        }
+  const handleSubmit = async (
+    values: {
+      todo: string;
+      user_id: string;
+    },
+    setSubmitting: (value: boolean) => void
+  ) => {
+    if (type == "add") {
+      try {
+        setSubmitting(true);
+        const response = await createTodoAPI(values);
+        toast.success(response?.data?.message);
+        dispatch(fetchTodosByUserID(userId));
+        setModalOpen(false);
+      } catch (error) {
+        console.log("Error in todo creation", error);
+        setErrorMessage("Something went wrong!");
+      } finally {
+        setSubmitting(false);
+      }
+    } else if (type == "edit") {
+      try {
+        setSubmitting(true);
+        const response = await updateTodoAPI(values, data?.id!);
+        toast.success(response?.data?.message);
+        dispatch(fetchTodosByUserID(userId));
+        setModalOpen(false);
+      } catch (error) {
+        console.log("Error in todo creatigon", error);
+        setErrorMessage("Something went wrong!");
+      } finally {
+        setSubmitting(false);
+      }
     }
+  };
 
   const formik = useFormik({
     initialValues: {
-     user_id : userId,
-     todo : data?.todo || "",
+      user_id: userId,
+      todo: data?.todo || "",
     },
     validationSchema: todoSchema,
-    onSubmit: (values) => {
-      handleSubmit(values);
+    onSubmit: (values, { setSubmitting }) => {
+      handleSubmit(values, setSubmitting);
     },
   });
 
-   
   return (
     <>
-     <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
-        <Input 
+      <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
+        <Input
           name="todo"
           type="text"
           label="Todo"
@@ -73,21 +79,30 @@ const TodoForm = ({type, data, setModalOpen}: TodoFormProps) => {
           type="submit"
           className="bg-cyan-500 mt-5 uppercase tracking-wider"
         >
-          {type == "edit" ? "Edit" : "Create"}
+          {!formik?.isSubmitting && type === "edit"
+            ? "Edit"
+            : !formik?.isSubmitting && type === "add"
+            ? "Create"
+            : formik.isSubmitting && type === "edit"
+            ? "Editing..."
+            : formik?.isSubmitting && type === "add"
+            ? "Creating..."
+            : null}
         </Button>
       </form>
       {errorMessage && (
         <span className="text-red-600 text-sm text-center font-medium ml-1">
           {errorMessage}
         </span>
-      )}</>
-  )
+      )}
+    </>
+  );
+};
+
+interface TodoFormProps {
+  type: "edit" | "add";
+  data?: ITodo | null;
+  setModalOpen: (value: boolean) => void;
 }
 
-interface TodoFormProps{
-    type : "edit" | "add";
-    data ?: ITodo | null
-    setModalOpen:(value : boolean)=> void
-}
-
-export default TodoForm
+export default TodoForm;
